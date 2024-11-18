3fbf5dcc727ce3e36344b75980df4b3d5694fc02
Aaditya <aadityakuamr518@gmail.com>
2024-11-19 01:49:15 +0530

    adding navbar

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const ejsMate=require("ejs-mate");
+const methodOverride=require("method-override");
 const port=7013;
 
 app.set("views",path.join(__dirname,"view"));
 app.set("view engine","ejs");
 
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "PUBLIC")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("./HOME/index.ejs");
     // res.send("working");
 })
 
