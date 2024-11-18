aed5dff3f1ae3c2582829481e12b310db5ee5da3
Aaditya <aadityakuamr518@gmail.com>
2024-11-19 01:32:14 +0530

    adding

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express=require("express");
 const app=express();
+const path=require("path");
 const port=7013;
+
+app.set("views",path.join(__dirname,"view"));
+app.set("view engine","ejs");
+
 app.get("/",(req,res)=>{
-    res.send("working");
+    res.render("index.ejs");
+    // res.send("working");
 })
 
 app.listen(port,(req,res)=>{
