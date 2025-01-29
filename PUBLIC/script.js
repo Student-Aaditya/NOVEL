8d7d81f04f4413897fd2bea0f675c3ab7f277c2e
Aaditya <aadityakuamr518@gmail.com>
2025-01-30 01:20:28 +0530

    adding

---
+++ PUBLIC/script.js
@@ -0,0 +1,16 @@
+let input = document.querySelector("#email"); 
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function() {
+    let email = input.value;
+    if (email.includes('@gmail.com')) {
+        console.log("valid");
+    } else {
+        console.log("invalid");
+        
+    }
+});
+
+module.exports = function Check(inputValue) {
+    return inputValue.includes('@gmail.com');
+};
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path=require("path");
 const ejsMate=require("ejs-mate");
 const methodOverride=require("method-override");
 const port=7013;
+const mongoose = require("mongoose");
+// const Check=require("./script.js");
 
 app.set("views",path.join(__dirname,"view"));
 app.set("view engine","ejs");
@@ -12,6 +14,19 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "PUBLIC")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/blog");
+
+}
+main().
+    then(() => {
+        console.log("sucessful connection");
+    }).catch((err) => {
+        console.log(err);
+    })
+
 
 app.get("/",(req,res)=>{
     res.render("./HOME/index.ejs");
@@ -25,7 +40,24 @@ app.get("/demo",(req,res)=>{
 app.get("/sign",(req,res)=>{
     res.render("./SIHNLOG/sign.ejs");
 })
+app.get("/log",(req,res)=>{
+    res.render("./SIHNLOG/log.ejs");
+})
 
+app.get("/contact",(req,res)=>{
+    res.render("./HOME/contact.ejs")
+})
+
+//store
+app.get("/store",(req,res)=>{
+    res.render("./HOME/store.ejs");
+})
+
+
+
+app.post("/login",(req,res)=>{
+    res.render("./Home/index.ejs");
+})
 app.post("/sign-up",(req,res)=>{
     res.render("./HOME/index.ejs");
 })
